0e41cd67cd6e27f8b33b54dfb9e44724a3540b1a
Carissa Holmes <carissaholmes@Mac.lan>
2026-05-15 18:10:12 -0500

    Add Trap Hip Hop sample audio

--- lib/sampleTracks.ts
+++ lib/sampleTracks.ts
@@ -2,7 +2,7 @@ export const sampleTracks = [
   { label: "Country Rock", url: "/samples/country-rock-sample.mp3" },
   { label: "Alt Rock", url: "/samples/alt-rock-sample.mp3" },
   { label: "Pop Punk", url: "/samples/pop-punk-sample.mp3" },
-  { label: "Trap / Hip Hop", url: "" },
+  { label: "Trap / Hip Hop", url: "/samples/trap-hip-hop-sample.mp3" },
   { label: "Hard Rock/Metal", url: "/samples/rap-rock-metal-sample.mp3" },
 ];
 
