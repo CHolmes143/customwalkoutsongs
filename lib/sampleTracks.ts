7996fc6273b1576f0a93b86ff7118f0768383409
Carissa Holmes <carissaholmes@Mac.lan>
2026-05-15 10:42:28 -0500

    Add alt rock sample track

--- lib/sampleTracks.ts
+++ lib/sampleTracks.ts
@@ -1,6 +1,6 @@
 export const sampleTracks = [
   { label: "Country Rock", url: "/samples/country-rock-sample.mp3" },
-  { label: "90's/2000's Alt Rock", url: "" },
+  { label: "90's/2000's Alt Rock", url: "/samples/alt-rock-sample.mp3" },
   { label: "BPM/Dance", url: "" },
   { label: "Pop Punk", url: "" },
   { label: "Trap / Hip Hop", url: "" },
