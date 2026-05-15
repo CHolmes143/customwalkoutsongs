12b574f7f32115aa58cb7678701695fc4aff08b8
Carissa Holmes <carissaholmes@Mac.lan>
2026-05-15 10:53:03 -0500

    Add pop punk sample track

--- lib/sampleTracks.ts
+++ lib/sampleTracks.ts
@@ -2,7 +2,7 @@ export const sampleTracks = [
   { label: "Country Rock", url: "/samples/country-rock-sample.mp3" },
   { label: "90's/2000's Alt Rock", url: "/samples/alt-rock-sample.mp3" },
   { label: "BPM/Dance", url: "" },
-  { label: "Pop Punk", url: "" },
+  { label: "Pop Punk", url: "/samples/pop-punk-sample.mp3" },
   { label: "Trap / Hip Hop", url: "" },
   { label: "Rap Rock/Metal", url: "/samples/rap-rock-metal-sample.mp3" },
 ];
